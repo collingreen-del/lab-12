63477de94c974fbca417cd62feda362e2ab94156
collingreen-del <collingreen@u.boisestate.edu>
2026-04-14 14:04:41 -0600

    Implement one-hot state machine module/ first half of working code (?)

--- onehot.v
+++ onehot.v
@@ -1,1 +1,19 @@
-// Implement one-hot state machine
+module onehot(
+    input  w,
+    input  clk,
+    input  reset,
+    output z,
+    output Ast,
+    output Bst,
+    output Cst,
+    output Dst,
+    output Est
+);
+    wire An, Bn, Cn, Dn, En;
+ 
+    
+    dff Adff(.Default(1'b1), .D(An), .clk(clk), .reset(reset), .Q(Ast));
+    dff Bdff(.Default(1'b0), .D(Bn), .clk(clk), .reset(reset), .Q(Bst));
+    dff Cdff(.Default(1'b0), .D(Cn), .clk(clk), .reset(reset), .Q(Cst));
+    dff Ddff(.Default(1'b0), .D(Dn), .clk(clk), .reset(reset), .Q(Dst));
+    dff Edff(.Default(1'b0), .D(En), .clk(clk), .reset(reset), .Q(Est));
