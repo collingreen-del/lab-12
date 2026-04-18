1fdf71178e408f32bcaaafd4e20c4fe798182ace
collingreen-del <collingreen@u.boisestate.edu>
2026-04-17 23:03:53 -0600

    Clean up comments in binary.v
    Removed state encoding comments and next-state logic comments for clarity.

--- binary.v
+++ binary.v
@@ -7,22 +7,14 @@ module binary(
 );
     wire [2:0] Next;
  
-    // State encoding: A=000, B=001, C=010, D=011, E=100
-    // State[2]=z_bit (MSB), State[1]=y_bit, State[0]=x_bit (LSB)
-    // Reset to A=000: all Default=0
+   
     dff bit0(.Default(1'b0), .D(Next[0]), .clk(clk), .reset(reset), .Q(State[0]));
     dff bit1(.Default(1'b0), .D(Next[1]), .clk(clk), .reset(reset), .Q(State[1]));
     dff bit2(.Default(1'b0), .D(Next[2]), .clk(clk), .reset(reset), .Q(State[2]));
  
-    // Output: z=1 in state C (010) or E (100)
     assign z = State[2] | (State[1] & ~State[0]);
  
-    // Next-state logic (KMap-minimized, variables: z=State[2], y=State[1], x=State[0], w)
-    //
-    // X_next (LSB) = ~z&~y&~x | ~z&~y&w | y&~x&w | y&x&~w | z&~y&~w
-    // Y_next (mid) = ~z & (~x&(y|w) | ~y&x)
-    // Z_next (MSB) = w&~z&y&x | w&z&~y&~x
- 
+   
     wire zb, y, x;
     assign zb = State[2];
     assign y = State[1];
